a3ebf21de87b9694053a74574d853396433f7eec
이주광 <sute__@naver.com>
2023-07-25 23:51:43 +0900

    AOS-8 coteste

--- app/src/main/java/com/kt/jiratest/MainActivity.kt
+++ app/src/main/java/com/kt/jiratest/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JiraTestTheme {
-        Greeting("Androidfgdfgdfgd")
+        Greeting("Androidfgdfgdfdfghfdghdfgd")
     }
 }
