9da7d84c25b644139dbbcfaf717a36d2e164ad54
이주광 <sute__@naver.com>
2023-07-25 21:17:28 +0900

    test

--- app/src/main/java/com/kt/jiratest/MainActivity.kt
+++ app/src/main/java/com/kt/jiratest/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name! JJTJT")
 }
 
 @Preview(showBackground = true)
