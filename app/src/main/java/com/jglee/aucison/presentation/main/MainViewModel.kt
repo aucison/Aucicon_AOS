13d6b4af3038db003d7cf6e783d2c7615102cf6b
이주광 <sute__@naver.com>
2023-08-15 21:51:30 +0900

    AUC-40 툴바 검색필드 재작성

--- app/src/main/java/com/jglee/aucison/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/jglee/aucison/presentation/main/MainViewModel.kt
@@ -2,10 +2,28 @@ package com.jglee.aucison.presentation.main
 
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterNotNull
 
 class MainViewModel: ViewModel() {
+    var _bestItemList = MutableStateFlow<List<Color>?>(null)
+    val bestItemList get() = _bestItemList.filterNotNull()
 
-    fun requestBanner(): List<Color> {
-        return listOf(Color.Blue, Color.Red, Color.Green)
+    private var _mainBannerList = MutableStateFlow<List<Color>?>(null)
+    val mainBannerList get() = _mainBannerList.filterNotNull()
+
+    private var _newItemList = MutableStateFlow<List<Color>?>(null)
+    val newItemList get() = _newItemList.filterNotNull()
+
+    fun requestBestItemList() {
+        _bestItemList.value = listOf(Color.Blue, Color.Red, Color.Green)
+    }
+
+    fun requestMainBanner() {
+        _mainBannerList.value = listOf(Color.Blue, Color.Red, Color.Green)
+    }
+
+    fun requestNewItemList() {
+        _newItemList.value = listOf(Color.Blue, Color.Red, Color.Green)
     }
 }
