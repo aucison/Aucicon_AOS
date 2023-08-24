449e0522bd5028abf8728c701f39351f96612d00
이주광 <jglee@inavi.kr>
2023-08-24 17:47:18 +0900

    마켓 페이지 그리드 정렬방식 변경

--- app/src/main/java/com/jglee/aucison/presentation/market/MarketPage.kt
+++ app/src/main/java/com/jglee/aucison/presentation/market/MarketPage.kt
@@ -1,9 +1,12 @@
 package com.jglee.aucison.presentation.market
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -15,9 +18,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -32,7 +37,10 @@ fun MarketPage() {
     val viewModel = viewModel<MarketViewModel>().apply { 
         requestMarketItems()
     }
-    val marketItems = viewModel.marketItems.collectAsState(initial = listOf())
+    val marketItems = viewModel.marketItems.collectAsState(initial = listOf(
+        Product(),
+        Product()
+    ))
 
     Column {
         MarketCategoryTab()
@@ -86,11 +94,21 @@ fun MarketCategoryTab() {
 fun GridMarketItemList(itemList: List<Product>) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(150.dp),
-        horizontalArrangement = Arrangement.spacedBy(10.dp),
-        verticalArrangement = Arrangement.spacedBy(10.dp)
+        modifier = Modifier.fillMaxWidth(),
     ) {
         items(itemList) { item ->
-            ProductItem(product = item)
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.padding(bottom = 10.dp)
+            ) {
+                ProductItem(product = item)
+            }
         }
     }
+}
+
+@Composable
+@Preview
+fun MarketPreview() {
+    MarketPage()
 }
